228da304403a9e13ce998b150df43bd4d8abd885
ajaysaini253 <ajaykumar2000724@gmail.com>
2025-08-01 13:29:49 +0530

    changes

--- src/components/home/CustomERP/customERP.js
+++ src/components/home/CustomERP/customERP.js
@@ -26,7 +26,7 @@ const CustomERP = () => {
 
                                 <Fade direction='up' triggerOnce={false} duration={1200} delay={9}>
                                     <div className='my-3'>
-                                        <h2 className="title wow fadeInUp webbaseerp_heading fs-1 text-start" data-wow-duration="600ms">
+                                        <h2 className="title wow fadeInUp webbaseerp_heading fs-3 text-start" data-wow-duration="600ms">
                                            Dedicated Customer <span className="yllow_txt">Support – </span> We're Here for You
                                         </h2>
                                     </div>
@@ -34,52 +34,52 @@ const CustomERP = () => {
 
                                 <Fade direction='up' triggerOnce={false} duration={1400} delay={9}>
                                     <div className='my-2'>
-                                        <p className='text-justify text-secondary'>
+                                        <p className='text-justify text-secondary fs-6'>
                                            At SoftTrade, we believe great software is only as good as the support behind it. That’s why we offer reliable, fast, and expert customer support to help you every step of the way.
                                               </p>
                                     </div>
                                 </Fade>
                             <Fade direction='up' triggerOnce={false} duration={1800} delay={9}>
-                                <h3 className="text-black">What You Can Expect :</h3>
+                                <h3 className="text-black fs-5">What You Can Expect :</h3>
                                 <ul className="list-unstyled mt-3">
                                     <li className="d-flex align-items-start mb-2">
-                                        <img src={icon1} alt="check" className="me-2" />
+                                        <img src={icon1} alt="check" width={16} className="me-2" />
                                        <div  > 
-                                        <h5 className="text-black mb-1 d-inline-block">Guaranteed Assistance :- </h5>
-                                        <span className="text-gray mb-0">
+                                        <h5 className="text-black mb-1 d-inline-block support-list-heading">Guaranteed Assistance :- </h5>
+                                        <span className="text-gray mb-0 support-list-span">
                                         &nbsp; We stand by our commitment to resolve your issues quickly and efficiently – with guaranteed response times.
                                         </span>
                                    </div>
                                     </li>
                                     <li className="d-flex align-items-start mb-2">
-                                        <img src={icon1} alt="check" className="me-2" />
+                                        <img src={icon1} alt="check" width={16} className="me-2" />
                                         <div  > 
-                                        <h5 className="text-black mb-1 d-inline-block">Multiple Support Channels :- </h5>
-                                        <span className="text-gray mb-0">
+                                        <h5 className="text-black mb-1 d-inline-block support-list-heading">Multiple Support Channels :- </h5>
+                                        <span className="text-gray mb-0 support-list-span">
                                         &nbsp; Reach us via Phone, WhatsApp, Remote Desktop, or Chat – whichever suits you best.
                                         </span>
                                    </div>
                                     </li>
                                     <li className="d-flex align-items-start mb-2">
-                                        <img src={icon1} alt="check" className="me-2" />
+                                        <img src={icon1} alt="check" width={16} className="me-2" />
                                        <div  > 
-                                        <h5 className="text-black mb-1 d-inline-block">Expert Technical Team :- </h5>
-                                        <span className="text-gray mb-0">
+                                        <h5 className="text-black mb-1 d-inline-block support-list-heading">Expert Technical Team :- </h5>
+                                        <span className="text-gray mb-0 support-list-span">
                                        &nbsp; Our trained and experienced support staff are ready to assist with setup, troubleshooting, customization, and training.
                                         </span>
                                    </div>
                                     </li>
                                     <li className="d-flex align-items-start mb-2">
-                                        <img src={icon1} alt="check" className="me-2" />
+                                        <img src={icon1} alt="check" width={16} className="me-2" />
                                        <div  > 
-                                        <h5 className="text-black mb-1 d-inline-block">Real-Time Remote Support :- </h5>
-                                        <span className="text-gray mb-0">
+                                        <h5 className="text-black mb-1 d-inline-block support-list-heading">Real-Time Remote Support :- </h5>
+                                        <span className="text-gray mb-0 support-list-span">
                                         &nbsp; Get real-time help through remote desktop tools to resolve issues without waiting. We don't prefer ticketing system.
                                         </span>
                                    </div>
                                     </li>
                                     {/* <li className="d-flex align-items-start">
-                                        <img src={icon1} alt="check" className="me-2" />
+                                        <img src={icon1} alt="check" width={16} className="me-2" />
                                         Dedicated Support Dashboard with Analytics
                                     </li> */}
                                 </ul>
